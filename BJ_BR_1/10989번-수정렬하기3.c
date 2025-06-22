1c26b08dfdc609e37da15ef8620cb2ced375ef07
JSA <118621159+SUN-AAA@users.noreply.github.com>
2025-06-23 00:43:45 +0900

    Add files via upload

---
+++ BJ_BR_1/10989번-수정렬하기3.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int n, x;
+    scanf("%d", &n);
+
+    int count[10001] = { 0 };
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        count[x]++;
+    }
+
+    for (int i = 1; i <= 10000; i++) {
+        while (count[i]--) {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
